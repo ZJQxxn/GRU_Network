88bb2e58471e86eb17cf6f39901fc2222f4d025e
Jiaqi Zhang <zhangjiaqi_seu@outlook.com>
2020-07-03 20:36:17 +0800

    Update behavioral timescale analysis.

--- BehavioralTimescaleAnalysis.py
+++ BehavioralTimescaleAnalysis.py
@@ -286,7 +286,100 @@ def analysis(choices, reward_probability, method, block_size = 70):
 
 
 def correlation():
+    #TODO: multiple behavioral timescale
     pass
+
+
+# =========================================================
+#                   WITHOUT   CHOICES
+# =========================================================
+
+def negativeLogLikelihoodWithoutChoice(param, rewards, choices, return_trajectory = False):
+    '''
+    Compute the negative log likelihood of data.
+    :param param: Parameters (alpha, beta, gamma, omega)
+    :param choices: Choices of validation.
+    :param rewards: Rewards of validation.
+    :param return_trajectory: Boolean value determine whether return the trajectory of probability  and value function.
+    :return: If return_trajectory is true: return negative log likelihood, value function trajectory, probability trajectory.
+    '''
+    alpha = param[0]
+    omega = param[1]
+    choices_num = 3 # TODO: for three-armed-bandit task; generalize later
+    trials_num = len(rewards)
+
+    reward_value = np.zeros((choices_num, ))
+    value_trajectory = []
+    prob_trajectory = []
+
+    nll = 0 # negative log likelihood
+    # param_val = param.valuesdict()
+    for trial in range(trials_num):
+        choice = int(choices[trial]) - 1
+        reward = int(rewards[trial])
+        # reward-dependent value Q_{t+1}(x) = (1-alpha) * Q_{t}(x) + alpha * Rew_t
+        reward_value[choice] = (1 - alpha) * reward_value[choice] + alpha * reward
+        value_trajectory.append(reward_value)
+        # negative log likelihood
+        weighted_overall_value = omega * reward_value
+        exp_overall_value = np.exp(weighted_overall_value) + 1e-5
+        log_prob = weighted_overall_value[choice] - np.log(np.sum(exp_overall_value))
+        nll += (- log_prob)
+        prob_trajectory.append(exp_overall_value / np.sum(exp_overall_value))
+    if not return_trajectory:
+        return nll
+    else:
+         return (nll, value_trajectory, prob_trajectory)
+
+
+def MLEWithoutChoice(logFilename, dataFilename, block_size = 70):
+    '''
+        Maximize likelihood estimation without choices for learning the parameters.
+        :param logFilename: Filename of the validation log.
+        :param dataFilename: Filename of the validation dataset.
+        :return: VOID
+        '''
+    # Load data; Pre-computing
+    logFile = h5py.File(logFilename, 'r')
+    choices = logFile['choice'].value.squeeze()
+    rewards = logFile['reward'].value.squeeze()
+    num_trials = len(choices)
+    reward_prob = loadmat(dataFilename)['data_ST_Brief'][0][0][0]
+    for index in range(1, num_trials):
+        if choices[index] > 3:  # unfinished trial
+            choices[index] = choices[index - 1]
+            rewards[index] = int(np.random.uniform(0, 1, 1) < reward_prob[int(choices[index] - 1)][index])
+    print("Number of trials (samples): %d" % num_trials)
+    # Create parameters with constraints
+    bounds = [[0, 1], [None, None]]
+    params = np.array([0.5, 1])
+    func = lambda parameter: negativeLogLikelihoodWithoutChoice(parameter, choices, rewards)
+    success = False
+    if not success:
+        res = scipy.optimize.minimize(
+            func,
+            x0=params,
+            method="SLSQP",
+            bounds=bounds,
+            tol=1e-8  # improve convergence
+        )
+        success = res.success
+        if not success:
+            print("Fail. Retry...")
+    # print("Success : ", res.success)
+    print("Estimated Parameter (alpha, omega): ", res.x)
+    # Test the estimation
+    _, value_function, estimation_prob = negativeLogLikelihoodWithoutChoice(res.x, choices, rewards, return_trajectory=True)
+    estimation_choices = np.array([np.argmax(each) for each in estimation_prob]) + 1
+    correct_rate = np.sum(estimation_choices == choices) / len(choices)
+    print("Estimation correct rate : ", correct_rate)
+    # Write to file
+    np.save("Behavioral-Analysis-Result/MLE_withoutchoice_choice_estimation.npy", estimation_prob)
+    np.save("Behavioral-Analysis-Result/MLE_withoutchoice_parameter_estimation.npy", res.x)
+    # Plot the estimated choices
+    analysis(estimation_choices, reward_prob, "MLE without choice", block_size)
+
+
 
 
 if __name__ == '__main__':
@@ -296,9 +389,13 @@ if __name__ == '__main__':
     testing_data_filename = path + "data/RewardAffect_ThreeArmed_TestingSet-2020_05_01-blk70-reverseblk0-noise-1.mat"
 
     # # MLE for parameter estimation
-    print("="*10, " MLE ", "="*10)
-    MLE(validation_log_filename, testing_data_filename, block_size = 70)
+    # print("="*10, " MLE ", "="*10)
+    # MLE(validation_log_filename, testing_data_filename, block_size = 70)
+    #
+    # # MSE for parameter estimation
+    # print("="*10, " MSE ", "="*10)
+    # MEE(validation_log_filename, testing_data_filename, block_size = 70)
 
-    # MSE for parameter estimation
-    print("="*10, " MSE ", "="*10)
-    MEE(validation_log_filename, testing_data_filename, block_size = 70)
+    # MLE without choices for parameter estimation
+    print("=" * 10, " MLE without choices ", "=" * 10)
+    MLEWithoutChoice(validation_log_filename, testing_data_filename, block_size=70)
